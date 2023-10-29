ab1bcf782e655fe285c4717cfb210e57c5b56bf8
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2023-10-29 23:01:18 +0530

    feat: Add middleware to handle json body parse errors

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const app = express()
 // init middleware
 app.use(express.json())
 
+// if json body failed to parse this middleware will handle error  
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        console.error(err);
+        return res.status(400).send({ status: 400, message: err.message }); // Bad request
+    }
+    next();
+});
+
 // Routes
 app.use('/', routes(config))
 
